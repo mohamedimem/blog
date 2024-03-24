b0f62db489e6518cf2afd488cf7980052e8a74a1
mohamedimem <imem.hamdi@etudiant-isi.utm.tn>
2024-03-24 17:41:46 +0100

    improved

--- src/js/nav.js
+++ src/js/nav.js
@@ -5,3 +5,28 @@ navLinks.forEach((link) => {
         link.setAttribute("aria-current", "page");
     }
 })
+
+
+
+function trackEmailSignup(e, t, a, i) {
+    const n = document.getElementById(e);
+    if (!n) return console.warn(`form ${e} not found`);
+    const s = "invalid",
+        r = "error-msg",
+        l = !n.hasAttribute("novalidate"),
+        o = n.querySelector('input[type="email"]'),
+        d = () => n.getElementsByClassName(r)[0] ? .remove();
+    o.addEventListener("focus", (() => { n.classList.remove(s), d() })), n.addEventListener("submit", (e => {
+        e.preventDefault();
+        if (!n.checkValidity()) {
+            if (d(), !l) {
+                const e = document.createElement("p");
+                e.classList.add(r), e.innerText = "Please enter a valid email address", o.after(e)
+            }
+            return void n.classList.add(s)
+        }
+        let c = !1;
+        const u = () => { c || (c = !0, n.submit()) };
+        window.plausible ? (plausible(i, { callback: u, props: { origin: a, variant: t } }), setTimeout(u, 1e3)) : u()
+    }))
+}
