91736e7ff1a00a34b3b25d9c0959f870970e643e
mohamedimem <imem.hamdi@etudiant-isi.utm.tn>
2024-03-25 19:56:24 +0100

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from "@astrojs/mdx";
-import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
