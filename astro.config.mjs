a68270445ea82a52b38502aa53b2d096104b8224
mohamedimem <imem.hamdi@etudiant-isi.utm.tn>
2024-03-25 18:37:43 +0100

    optimize image netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from "@astrojs/mdx";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+    output: 'server',
+    adapter: netlify({
+        imageCDN: false,
+    }),
+
+    integrations: [mdx()]
 });
